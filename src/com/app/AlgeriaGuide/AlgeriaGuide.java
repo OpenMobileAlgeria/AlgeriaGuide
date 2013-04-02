5db03aaecaef0f8af8501feb244b9a803ea3f734
hacentech <hacentech@gmail.com>
2013-04-02 22:10:05 +0100

    first app

---
+++ src/com/app/AlgeriaGuide/AlgeriaGuide.java
@@ -0,0 +1,21 @@
+/*
+     
+ */
+
+package com.app.AlgeriaGuide;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class AlgeriaGuide extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl(Config.getStartUrl());
+        //super.loadUrl("file:///android_asset/www/index.html")
+    }
+}
+
